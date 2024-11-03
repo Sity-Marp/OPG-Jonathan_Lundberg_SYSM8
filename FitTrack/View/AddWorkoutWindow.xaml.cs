0303fb9cad58bf0d8ab5c7d22216bb204fe89013
Sity-Marp <88293493+Sity-Marp@users.noreply.github.com>
2024-11-03 13:29:06 +0100

    Revert AddWorkout back to temp UI

--- FitTrack/View/AddWorkoutWindow.xaml.cs
+++ FitTrack/View/AddWorkoutWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using FitTrack.ViewModel;
 
 namespace FitTrack.View
 {
@@ -22,6 +23,7 @@ namespace FitTrack.View
         public AddWorkoutWindow()
         {
             InitializeComponent();
+
         }
     }
 }
--- FitTrack/obj/Debug/net8.0-windows/FitTrack.AssemblyInfo.cs
+++ FitTrack/obj/Debug/net8.0-windows/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071b336b6d0291864b4b2783b42fd8832d4bd4d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc45c7563c62ca267662af50a3764bf6c6d62639")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
